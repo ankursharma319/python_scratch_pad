cf99d724ae4947f5903a524c124392c1fa1f02c8
Ankur Sharma <ankurs4@cisco.com>
2022-08-22 15:30:50 +0200

    dp: paranthesize matrix multiplication

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -68,3 +68,71 @@ def justify_words(page_width: int, words: list) -> list [list [str]]:
         slices_memo=slices_memo
     )
     return _slices_to_result(words, slices)
+
+
+def _cost_of_matrix_multiplication(i,j,k):
+    """
+    approx cost of multiplying matrices A_ij B_jk
+    i is number of rows
+    k is number of columns
+    j is number of rows and columns
+    """
+    return j*i*k
+
+def _get_matrix_mult_result_size(matrices):
+    assert len(matrices) > 0
+    return matrices[0][0], matrices[-1][1]
+
+def _paranths_to_result(matrices, paranths_memo, i, j):
+    assert j > i
+    if j - i == 1:
+        return matrices[i]
+    if j - i == 2:
+        return matrices[i:j]
+    k = paranths_memo[(i, j)]
+    left = _paranths_to_result(matrices=matrices, paranths_memo=paranths_memo, i=i, j=k)
+    right = _paranths_to_result(matrices=matrices, paranths_memo=paranths_memo, i=k, j=j)
+    result = [left, right]
+    return result
+
+def _paranthesize(i, j, matrix_sizes, cost_memo, paranths_memo):
+    if (i,j) in cost_memo:
+        assert (i,j) in paranths_memo
+        return cost_memo[(i,j)]
+    current_min_cost = inf
+    current_min_k = inf
+    for k in range(i+1,j):
+        assert k != i
+        assert k != j
+        left_matrix_size = _get_matrix_mult_result_size(matrix_sizes[i:k])
+        right_matrix_size = _get_matrix_mult_result_size(matrix_sizes[k:j])
+        assert left_matrix_size[1] == right_matrix_size[0], f"invalid shapes of matrices to be multiplied"
+        if k-i <= 1:
+            left_cost = 1
+        else:
+            left_cost = _paranthesize(i=i, j=k, matrix_sizes=matrix_sizes, cost_memo=cost_memo, paranths_memo=paranths_memo)
+        if j-k <= 1:
+            right_cost = 1
+        else:
+            right_cost = _paranthesize(i=k, j=j, matrix_sizes=matrix_sizes, cost_memo=cost_memo, paranths_memo=paranths_memo)
+        outer_cost = _cost_of_matrix_multiplication(left_matrix_size[0],left_matrix_size[1],right_matrix_size[1])
+        total_cost = outer_cost + left_cost + right_cost
+        if total_cost < current_min_cost:
+            current_min_cost = total_cost
+            current_min_k = k
+    cost_memo[(i,j)] = current_min_cost
+    paranths_memo[(i,j)] = current_min_k
+    return cost_memo[(i,j)]
+
+def paranthesize_matrix_multiplication(matrix_sizes):
+    cost_memo = {}
+    paranths_memo = {}
+    _ = _paranthesize(
+        i=0,j=len(matrix_sizes),
+        matrix_sizes=matrix_sizes,
+        cost_memo=cost_memo,
+        paranths_memo=paranths_memo
+    )
+    return _paranths_to_result(
+        matrices=matrix_sizes, paranths_memo=paranths_memo, i=0, j=len(matrix_sizes)
+    )
--- test_dynamic_programming.py
+++ test_dynamic_programming.py
@@ -19,3 +19,27 @@ def test_word_justification_dp_complex():
         ["now"],
     ]
     assert expected_result == result
+
+def test_paranthesization_matrix_multipliciation_simple():
+    matrices = [(5,1),(1,5),(5,1)]
+    result = dp.paranthesize_matrix_multiplication(matrices)
+    expected_result = [(5,1), [(1,5), (5,1)]]
+    assert expected_result == result
+
+def test_paranthesization_matrix_multipliciation_complex():
+    matrices = [(5,25), (25,5), (5,1), (1,10), (10,3), (3,30), (30,6)]
+    result = dp.paranthesize_matrix_multiplication(matrices)
+    expected_result = [
+        [
+            (5, 25),
+            [(25, 5), (5, 1)]
+        ],
+        [
+            [
+                [(1, 10), (10, 3)],
+                (3, 30)
+            ],
+            (30, 6)
+        ]
+    ]
+    assert expected_result == result
